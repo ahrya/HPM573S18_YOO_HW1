a17077b3ee879fe712d0820cbd932ab7050468c0
ahrya <harim.yoo@yale.edu>
2018-01-23 22:58:40 -0500

    part1

---
+++ Q1.py
@@ -0,0 +1,28 @@
+##QUESTION1
+
+#x=17
+x=17
+
+#integer
+y1 = x
+print(type(y1))
+
+#float
+y2 = float(x)
+print(type(y2))
+
+#string
+y3 = "17"
+print(type(y3))
+
+#boolean
+y4 = bool(x<17)
+print(y4)
+print(type(y4))
+
+#2
+sentence = "The value of x is "
+text = sentence + y3 +"."
+print(text)
+
+
---
+++ Q3.py
@@ -0,0 +1,4 @@
+#iterative
+
+
+#recursive
